0f313d1f859cc1e0176a7f8e9c482cb4d788e9e9
nghiatrong <trongnghianguyen2904@gmail.com>
2023-01-05 20:47:00 +0700

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './component/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
+
+let logoutTimer;
 const App = () => {
-  return <div>App</div>;
-};
+  return (
+    <div style={{ position: 'relative' }}>
+      <Router>
+        <Navbar />
+        <div style={{ marginTop: '80px' }}>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={
+              <Login />
+            } />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  )
+}
 
-export default App;
+export default App
